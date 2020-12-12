550c97808f77a7d6c0e17b9db346b7cd492d5bec
Steffen <steffen.oppel@gmail.com>
2020-12-12 21:53:16 +0000

    added bird env dat extraction
    added env dat extraction for bird locations (for all seasons, even those when the birds were not tracked), and streamlined data summary by season

--- StHel_ENV_data_prep.r
+++ StHel_ENV_data_prep.r
@@ -22,6 +22,8 @@ require(rgeos)
 library(raster)
 library(trip)
 library(marmap)
+select<-dplyr::select
+filter<-dplyr::filter
 
 setwd("C:\\STEFFEN\\RSPB\\UKOT\\StHelena\\Science\\Birds\\seabirds")
 
@@ -34,6 +36,17 @@ setwd("C:\\STEFFEN\\RSPB\\UKOT\\StHelena\\Science\\Birds\\seabirds")
 bird.tracks<-fread("St Helena seabird tracking-5605684338234142518.csv")
 names(bird.tracks)
 dim(bird.tracks)
+head(bird.tracks)
+
+
+#create Spatial Points Data Frame 
+BIRDLOCS<-bird.tracks %>% mutate(year=year(timestamp), month=month(timestamp)) %>%
+  mutate(period=ifelse(month>7,"hot","cool")) %>%
+  mutate(period=paste(period, year, sep="_")) %>%
+  select(timestamp,`location-long`,`location-lat`,`individual-taxon-canonical-name`,`tag-local-identifier`,period) %>%
+  rename(LAT=`location-lat`,LONG=`location-long`)
+coordinates(BIRDLOCS)= ~ LONG + LAT
+
 
 
 
@@ -53,10 +66,19 @@ head(bathgrid)
 ####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
 ####~~~~~ 			LOAD ENVIRONMENTAL VARIABLES FROM MOVEBANK BACKGROUND				   ~~~~~~####
 ####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~####
+## this creates a file with 35 million rows, which is too big
+## calculating means in Raster Stack: https://gis.stackexchange.com/questions/222291/extracting-mean-of-multiple-raster-layers-using-r
+## for each of 4 tracking periods we average the raster values
+
+seasons<-data.frame(season='hot_2017',file=seq(1,24)) %>%
+  bind_rows(data.frame(season='cool_2018',file=seq(25,41))) %>%
+  bind_rows(data.frame(season='hot_2018',file=seq(42,61))) %>%
+  bind_rows(data.frame(season='cool_2019',file=seq(62,92)))
 
 setwd("C:\\STEFFEN\\RSPB\\UKOT\\StHelena\\Science\\Birds\\seabirds\\ENVBackground")
 subfolders<-list.dirs()
 env.bgr<-data.frame()
+env.bird<-data.frame()
 
 for (d in c(2:17,20,22:length(subfolders))){
   
@@ -65,37 +87,77 @@ for (d in c(2:17,20,22:length(subfolders))){
   ## list all the tif files
   grid.files<-list.files(pattern=".tif")
   env.bg.add<-data.frame()
+  env.bird.add<-data.frame()
   
   ## read in all files
-  for (f in 1:length(grid.files)){
+  ## 1-24 = 'hot_2017', 25-42='cool_2018', 43-63='hot_2018', 64-92='cool_2019'
+  
+  for (s in unique(seasons$season)){
+  
+  seasonfiles<-grid.files[seasons$file[seasons$season==s]]
+  #for (f in seasons$file[seasons$season]){
     
     ## EXTRACT DATE FROM FILE NAME
-    regexp <- "[[:digit:]]+"
-    adjfilename<-str_replace(grid.files[f], pattern=c("2 m|10 m|4km|8d|AR 1|3deg|5d"), replace="") ### this needs to be adjusted for some file/folder names that have 2m 8 d 10 m etc. in the name!
-    filedate<-ymd_hms(substr(str_extract(adjfilename, regexp),1,14)) 
+    #regexp <- "[[:digit:]]+"
+    #adjfilename<-str_replace(grid.files[f], pattern=c("2 m|10 m|4km|8d|AR 1|3deg|5d"), replace="") ### this needs to be adjusted for some file/folder names that have 2m 8 d 10 m etc. in the name!
+    #filedate<-ymd_hms(substr(str_extract(adjfilename, regexp),1,14)) 
     
     ## READ IN DATA
-    dat.in<-raster(grid.files[f])
-    out<-raster::as.data.frame(dat.in,xy=TRUE)
+    STACK1 <- stack(seasonfiles)
+    season.mean <- calc(STACK1, fun = mean, na.rm = T)
+    #dat.in<-raster(grid.files[f])
+    out<-raster::as.data.frame(season.mean,xy=TRUE)
     head(out)
 
     ## ADD THE VARIABLE AND THE DATE
     names(out)<-c("Long","Lat","value")
     env.bg.add<-out %>%
       mutate(variable=substr(subfolders[d],3,nchar(subfolders[d]))) %>%
-      mutate(time=filedate) %>%
+      #mutate(time=filedate) %>%
+      mutate(season=s) %>%
       bind_rows(env.bg.add)
-  }	# close loop over each file
+    
+    ## EXTRACT THE ENV DATA FOR BIRD LOCATIONS
+    bird.ras<-extract(season.mean,BIRDLOCS)
+    BIRDLOCS$value<-raster::as.data.frame(bird.ras,xy=TRUE)
+    env.bird.add<-as.data.frame(BIRDLOCS) %>%
+      mutate(variable=substr(subfolders[d],3,nchar(subfolders[d]))) %>%
+      mutate(season=s) %>%
+      rename(value=bird.ras) %>%
+      bind_rows(env.bird.add)
+
+  }	# close loop over each season
 
 env.bgr<-rbind(env.bgr,env.bg.add)
+env.bird<-rbind(env.bird,env.bird.add)
 
 } # close loop over each directory
 
 
 head(env.bgr)
 dim(env.bgr)
+unique(env.bgr$time)
 
+
+
+
+## SAVE AND EXPORT DATA ##
 setwd("C:\\STEFFEN\\RSPB\\UKOT\\StHelena\\Science\\Birds\\seabirds\\ENVBackground")
+save.image("StHel_EnvDat_Background.RData")
 fwrite(env.bgr,"StHel_EnvDat_Background.csv")
 
 
+
+##### AVERAGE ALL THE ENVIRONMENTAL VARIABLES FOR SEVERAL TIME PERIODS ###
+head(env.bgr)
+
+env.per<- env.bgr %>% mutate(year=year(time), month=month(time)) %>%
+  mutate(period=ifelse(month>7,"hot","cool")) %>%
+  mutate(period=paste(period, year, sep="_"))
+head(env.per)
+
+
+env.sum<-env.per %>% group_by(Long,Lat,period,variable) %>%
+  summarise(value=mean(value, na.rm=T))
+dim(env.sum)
+fwrite(env.sum,"StHel_EnvDat_Background.csv")
